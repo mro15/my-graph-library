65680c126a40b7dd6471af1554443137d8b63abf
Marcela Ribeiro de Oliveira <mro15@inf.ufpr.br>
2019-04-04 15:28:03 -0300

    start with graph

---
+++ text_graph/node_features.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+
+import gensim
+
+class NodeFeatures(object):
+    def __init__(self):
+        self.w2v_model = gensim.models.KeyedVectors.load_word2vec_format('../GoogleNews-vectors-negative300.bin', binary=True)
+
+    def edge_weight(self, v1, v2):
+        return self.w2v_model.similarity(v1, v2)
