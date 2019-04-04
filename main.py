da60c493db04bfda69fa2fc5c999571910f72473
Marcela Ribeiro de Oliveira <mro15@inf.ufpr.br>
2019-04-04 16:11:32 -0300

    w2v pre trained to calculate edge weights

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ from text_graph.node_features import NodeFeatures
 def main():
     g = TextGraph("Test")
     v = NodeFeatures()
-    sentence = "cat dog tree human car woman queen"
+    print("== MODEL LOADED ==")
+    sentence = "cat dog tree woman human car queen king"
     l_sentence = sentence.split()
     for s in l_sentence:
         g.add_vertex(s)
     for s in range(0, len(l_sentence)-1):
         w1 = l_sentence[s]
         w2 = l_sentence[s+1]
-        g.add_edge(w1, w2, v.edge_weight(w2, w2))
+        g.add_edge(w1, w2, v.edge_weight(w1, w2))
     g.plot_graph()
 
 if __name__ == "__main__":
--- text_graph/node_features.py
+++ text_graph/node_features.py
@@ -4,7 +4,12 @@ import gensim
 
 class NodeFeatures(object):
     def __init__(self):
-        self.w2v_model = gensim.models.KeyedVectors.load_word2vec_format('../GoogleNews-vectors-negative300.bin', binary=True)
+        self.w2v_model = gensim.models.KeyedVectors.load_word2vec_format('./GoogleNews-vectors-negative300.bin', binary=True)
 
     def edge_weight(self, v1, v2):
-        return self.w2v_model.similarity(v1, v2)
+        cos = self.w2v_model.similarity(v1, v2)
+        print(v1, v2, cos)
+        return cos
+
+    def vertex_w2v_features(self, v):
+        pass
