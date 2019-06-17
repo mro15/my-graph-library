07d879fa2b5f17211bcf933cc9653955ef07d0a5
Marcela Ribeiro de Oliveira <mro15@inf.ufpr.br>
2019-06-17 14:25:40 -0300

    first cnn tuned in polarity finished

--- cnn/my_cnn.py
+++ cnn/my_cnn.py
@@ -2,7 +2,7 @@
 
 import keras
 from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Flatten, Input
+from keras.layers import Dense, Dropout, Flatten, Input, BatchNormalization
 from keras.layers import Conv1D, GlobalMaxPooling1D, MaxPooling1D
 import numpy as np
 from keras.layers.merge import Concatenate
@@ -18,25 +18,32 @@ class My_cnn(object):
 
     def do_all(self):
         print(self.input_shape, self.train_x.shape)
-        filters = (2, 3, 5, 6, 8)
+        filters = (2, 3, 5, 7, 8)
         model_input = Input(shape=self.input_shape)
         conv_blocks = []
         for block_size in filters:
-            conv = Conv1D(filters=128, kernel_size=block_size, padding='valid', activation='tanh', strides=1)(model_input)
+            conv = Conv1D(filters=32, kernel_size=block_size, padding='valid', activation='tanh', strides=1)(model_input)
             conv = Dropout(0.2)(conv)
             conv = GlobalMaxPooling1D()(conv)
-            #conv = Flatten()(conv)
+            conv1 = Conv1D(filters=64, kernel_size=block_size, padding='valid', activation='tanh', strides=1)(model_input)
+            conv1 = Dropout(0.2)(conv1)
+            conv1 = GlobalMaxPooling1D()(conv1)
+            conv2 = Conv1D(filters=128, kernel_size=block_size, padding='valid', activation='tanh', strides=1)(model_input)
+            conv2 = Dropout(0.2)(conv2)
+            conv2 = GlobalMaxPooling1D()(conv2)
             conv_blocks.append(conv)
+            conv_blocks.append(conv1)
+            conv_blocks.append(conv2)
 
-        conc = Concatenate()(conv_blocks) if len(conv_blocks) > 1 else conv_blocks[0]
+        conc = Concatenate(axis=1)(conv_blocks) if len(conv_blocks) > 1 else conv_blocks[0]
+        conc = Dense(256, activation='relu')(conc)
         conc = Dropout(0.2)(conc)
-        conc = Dense(32, activation='relu')(conc)
         model_output = Dense(2, activation='softmax')(conc)
 
         model = Model(model_input, model_output)
         model.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
 
-        hist = model.fit(self.train_x, self.train_y, batch_size=32, epochs=20, shuffle=True, verbose=2, validation_data=(self.test_x, self.test_y))
+        hist = model.fit(self.train_x, self.train_y, batch_size=32, epochs=30, shuffle=True, verbose=2, validation_data=(self.test_x, self.test_y))
 
         score = model.evaluate(self.test_x, self.test_y)
 
---
+++ cnn_main.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python3
+
+import argparse
+from cnn.my_cnn import My_cnn
+import numpy as np
+import _pickle as pickle
+import sklearn
+
+def read_args():
+    parser = argparse.ArgumentParser(description="The parameters are:")
+    parser.add_argument('--dataset', type=str, choices=["imdb", "polarity"], help='dataset name', required=True)   
+    parser.add_argument('--method', type=str, choices=["node2vec", "gcn"], help='representation method', required=True)
+    parser.add_argument('--strategy', type=str, choices=["no_weight", "pmi"], help='representation method', required=True)
+    parser.add_argument('--window', type=int,  help='window size', required=True)
+    return parser.parse_args()
+
+def padding(train, test):
+    m_train = len(max(train, key = lambda i: len(i)))
+    m_test = len(max(test, key = lambda i: len(i)))
+    m_all = max(m_train, m_test)
+    pad = [0] * 50
+    for i in range(0, len(train)):
+        if len(train[i]) < m_all:
+            mult = m_all - len(train[i])
+            train[i]+= ([pad] * mult)
+    for i in range (0, len(test)):
+        if len(test[i]) < m_all:
+            mult = m_all - len(test[i])
+            test[i]+= ([pad] * mult)
+    print(m_all)
+    return train, test
+
+def main():
+    args = read_args()
+
+    with open(args.method + '_' + args.strategy + '_' + 'train_x.pkl', 'rb') as infile:
+        train_emb = pickle.load(infile)
+    with open(args.method + '_' + args.strategy + '_' + 'train_y.pkl', 'rb') as infile:
+        train_labels = pickle.load(infile)
+    with open(args.method + '_' + args.strategy + '_' + 'test_x.pkl', 'rb') as infile:
+        test_emb = pickle.load(infile)
+    with open(args.method + '_' + args.strategy + '_' + 'test_y.pkl', 'rb') as infile:
+        test_labels = pickle.load(infile)
+    
+    train_pad, test_pad = padding(train_emb, test_emb)
+    print(np.array(train_pad).shape)
+    print(np.array(test_pad).shape)
+
+    train_emb, train_labels = sklearn.utils.shuffle(train_emb, train_labels, random_state=0)
+    test_emb, test_labels = sklearn.utils.shuffle(test_emb, test_labels, random_state=0)
+
+    print(np.array(train_pad).shape)
+    print(np.array(test_pad).shape)
+
+    mcnn = My_cnn(train_emb, train_labels, test_emb, test_labels, (len(train_emb[0]),50), 2)
+    mcnn.do_all()
+
+if __name__ == "__main__":
+    main()
