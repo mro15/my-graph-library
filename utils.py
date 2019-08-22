eb3547560406bce3cea3cf642d967c9328b05ef6
Marcela Ribeiro de Oliveira <mro15@inf.ufpr.br>
2019-08-22 16:23:13 -0300

    add dice measure

--- utils.py
+++ utils.py
@@ -383,18 +383,18 @@ def graph_strategy_six(d, k):
     print("BUILDING GRAPHS FROM TRAIN DATASET")
     progress = tqdm(d.train_data)
     for i in progress:
-        total_words = len(i)
         g = TextGraph(d.dataset)
+        total_words = len(i)
+        word_frequency = word_count(i) #frequency of each word in sentence
         windows = []
         if total_words > k:
             windows += build_windows(i, k)
         else:
             windows.append(i)
         pair_windows = windows_in_pair(windows) #co-occurrence of (w1, w2)
-        word_frequency = word_count(i) #frequency of each word in sentence
-        for words, freq in pair_windows:
+        for words, freq in pair_windows.items():
             w1, w2 = words
-            dice = ((2*freq)/(word_frequency[w1]/word_frequency[w2]))
+            dice = ((2*freq)/(word_frequency[w1]+word_frequency[w2]))
             if dice >= 0:
                 g.add_vertex(w1)
                 g.add_vertex(w2)
@@ -402,6 +402,7 @@ def graph_strategy_six(d, k):
         if((len(list(pair_windows))<1) or (len(g.nodes())==0)):
             g.add_vertex(i[0])
         train_graphs.append(g.graph)
+        """
         #debug
         print("---- NODES ----")
         print(g.nodes())
@@ -409,6 +410,39 @@ def graph_strategy_six(d, k):
         print(g.edges())
         plot_graph(g.graph)
         exit()
+        """
     print("FINISHED GRAPHS FROM TRAIN DATASET")
+    print("BUILDING GRAPHS FROM TEST DATASET")
+    progress = tqdm(d.test_data)
+    for i in progress:
+        g = TextGraph(d.dataset)
+        total_words = len(i)
+        word_frequency = word_count(i) #frequency of each word in sentence
+        windows = []
+        if total_words > k:
+            windows += build_windows(i, k)
+        else:
+            windows.append(i)
+        pair_windows = windows_in_pair(windows) #co-occurrence of (w1, w2)
+        for words, freq in pair_windows.items():
+            w1, w2 = words
+            dice = ((2*freq)/(word_frequency[w1]+word_frequency[w2]))
+            if dice >= 0:
+                g.add_vertex(w1)
+                g.add_vertex(w2)
+                g.add_weight_edge(w1, w2, dice)
+        if((len(list(pair_windows))<1) or (len(g.nodes())==0)):
+            g.add_vertex(i[0])
+        test_graphs.append(g.graph)
+        """
+        #debug
+        print("---- NODES ----")
+        print(g.nodes())
+        print("---- EDGES ----")
+        print(g.edges())
+        plot_graph(g.graph)
+        exit()
+        """
+    print("FINISHED GRAPHS FROM TEST DATASET")
 
-
+    return train_graphs, test_graphs
