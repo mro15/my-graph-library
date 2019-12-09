bdbd93320f82279279f5e70881acd6539da4d705
Marcela Ribeiro de Oliveira <mro15@inf.ufpr.br>
2019-12-09 14:33:28 -0300

    update cnn_runner script

--- cnn_runner.sh
+++ cnn_runner.sh
@@ -1,14 +1,25 @@
 #! /bin/bash
 
-dim="100 300"
+dim="100"
+#dim="100 300"
 windows="4 7 20 12"
-strategies="no_weight pmi_2019 pmi_1990 dice llr chi_square"
+#strategies="no_weight pmi_2019 pmi_1990 dice llr chi_square"
+strategies="no_weight pmi_2019 pmi_1990 pmi_1990_all"
+datasets="20ng ohsumed webkb polarity"
+methods="node2vec"
+pooling="global_max"
 
-for d in $dim; do
-	for w in $windows; do
-		for s in $strategies; do
-			python3 cnn_main.py --dataset polarity --method node2vec --strategy $s --window $w --emb_dim $d
-			echo "python3 cnn_main.py --dataset polarity --method node2vec --strategy $s --window $w --emb_dim $d" >> executed_cnn.txt
+for dr in $datasets; do
+	for m in $methods; do
+		for d in $dim; do
+			for w in $windows; do
+				for s in $strategies; do
+					for p in $pooling; do
+						command="python3 cnn_main.py --dataset $dr --method $m --strategy $s --window $w --emb_dim $d --pool_type $p"
+						echo $command | bash >> logs/$dr/$m/$s.$w.$d.$p.txt
+					done
+				done
+			done
 		done
 	done
 done
