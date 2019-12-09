55ee9106a49ccdbcfbbd3e4af8d8fb559512968f
Marcela Ribeiro de Oliveira <mro15@inf.ufpr.br>
2019-12-09 14:42:07 -0300

    update cnn main

--- cnn_main.py
+++ cnn_main.py
@@ -17,14 +17,13 @@ def read_args():
     parser.add_argument('--window', type=int,  help='window size', required=True)
     parser.add_argument('--emb_dim', type=int,  help='embeddings dimension', required=True)
     parser.add_argument('--pool_type', type=str, choices=["max", "global_max"], help='pooling type', required=True)
-    parser.add_argument('--classes', type=int, help='number of classes', required=True)
     return parser.parse_args()
 
 def padding(train, test, dim):
     m_train = len(max(train, key = lambda i: len(i)))
     m_test = len(max(test, key = lambda i: len(i)))
     m_all = max(m_train, m_test)
-    print(m_all)
+    #print(m_all)
     pad = np.zeros(dim)
     sparse_all = []
     for i in range(0, len(train)):
@@ -58,8 +57,13 @@ def padding_and_truncate(sentences, dim, cut_point):
 
 def main():
     args = read_args()
-
+    classes = {
+            "polarity": 2,
+            "webkb": 4,
+            "ohsumed": 23,
+            "20ng": 20 }
     directory = "graphs/" + args.dataset + "-" + str(args.emb_dim) + "/"
+
     with open(directory + args.dataset + '_' + args.method + '_' + args.strategy + '_' + str(args.window) + '_' + 'train_x.pkl', 'rb') as infile:
         train_emb = pickle.load(infile)
     with open(directory + args.dataset + '_' + args.method + '_' + args.strategy + '_' + str(args.window) + '_' + 'train_y.pkl', 'rb') as infile:
@@ -68,27 +72,15 @@ def main():
         test_emb = pickle.load(infile)
     with open(directory + args.dataset + '_' + args.method + '_' + args.strategy + '_' + str(args.window) + '_' + 'test_y.pkl', 'rb') as infile:
         test_labels = pickle.load(infile)
-    
-
-    print(np.array(train_emb).shape)
-    print(np.array(test_emb).shape)
 
     tg_features.discover_sentences_size(train_emb+test_emb)
     tg_features.sentences_histogram(train_emb+test_emb, args.dataset)
     padding_cut = tg_features.sentences_percentile(train_emb+test_emb)
-    #all_x = padding_and_truncate(train_emb+test_emb, args.emb_dim, int(padding_cut))
-    all_x = padding(train_emb, test_emb, args.emb_dim)
+    all_x = padding_and_truncate(train_emb+test_emb, args.emb_dim, int(padding_cut))
+    #all_x = padding(train_emb, test_emb, args.emb_dim)
     all_y = np.concatenate((np.array(train_labels), np.array(test_labels)), axis=None)
 
-    train_emb = None
-    test_emb = None
-
-    print(np.array(all_x).shape)
-    print(np.array(all_y).shape)
-
-    mcnn = My_cnn(np.array(all_x), all_y, np.array(all_x[0]).shape, args.classes, args.pool_type)
-    all_x = None
-    all_y = None
+    mcnn = My_cnn(np.array(all_x), all_y, np.array(all_x[0]).shape, classes[args.dataset], args.pool_type)
     results, results_f1 = mcnn.do_all()
 
     directory = "results/" + args.dataset + "-" + str(args.emb_dim) + "/"
