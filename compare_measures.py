6cc9aa039b679d20ae194451b3660f3cf07a22df
Marcela Ribeiro de Oliveira <mro15@inf.ufpr.br>
2019-12-17 11:01:11 -0300

    refactoring script to compare measures

--- compare_measures.py
+++ compare_measures.py
@@ -15,7 +15,14 @@ def read_args():
     parser = argparse.ArgumentParser(description="The parameters are:")
     parser.add_argument('--dataset', type=str, choices=["imdb", "polarity", "mr", "webkb"], help='dataset name', required=True)   
     parser.add_argument('--window', type=int,  help='window size', required=True)
+    parser.add_argument('--strategy', action="append", help='methods to compare', required=True)
     return parser.parse_args()
+
+def get_legend(strategy):
+    legend_map = { "no_weight": "Sem peso",
+                "pmi_1990": "PMI (1990)",
+                "pmi_1990_all": "PMI (1990) ALL"}
+    return legend_map[strategy]
 
 def plot_boxplot(values, methods, name):
     fig1, ax1 = plt.subplots()
@@ -70,6 +77,33 @@ def edges_sub(a, b, method):
             number += i
     print(method, cont, number)
 
+#calculate mean and std for metrics
+def mean_and_std(edges, strategies):
+    mean = {}
+    std = {}
+    for s in strategies:
+        mean[s] = np.mean(edges[s])
+        std[s] = np.std(edges[s])
+    return mean, std
+
+def plot_cost(strategies, edges, window):
+    mean, std = mean_and_std(edges, strategies)
+    print(mean, std)
+    
+    mean_v = []
+    std_v = []
+    bar = []
+    for s in strategies:
+        mean_v.append(mean[s])
+        std_v.append(std[s])
+        bar.append(get_legend(s))
+    plt.errorbar(bar, mean_v, yerr=std_v, marker='s', capsize=5)
+    plt.xlabel("Métricas")
+    plt.ylabel("# arestas")
+    plt.title("Comparação # arestas médio," + " #janela: " + str(window))
+    plt.savefig("analysis/cost_mean_edges_" + str(window) + ".png")
+    plt.close()
+
 def main():
     args = read_args()
     if args.dataset == "polarity":
@@ -87,34 +121,47 @@ def main():
     else:
         print("Error: dataset name unknown")
         return 1
+
     print("PRE PROCESS: START")
     d.pre_process_data()
     print("PRE PROCESS: END")
 
-    all_edges_train, all_edges_test = utils.graph_strategy_one(d, args.window)
-    pmi_2019_edges_train, pmi_2019_edges_test = utils.graph_strategy_two(d, args.window)
-    pmi_1990_edges_train, pmi_1990_edges_test = utils.graph_strategy_three(d, args.window)
-    pmi_1990_all_edges_train, pmi_1990_all_edges_test = utils.graph_strategy_three_all(d, args.window)
-    dice_edges_train, dice_edges_test = utils.graph_strategy_four(d, args.window)
-    dice_all_edges_train, dice_all_edges_test = utils.graph_strategy_four_all(d, args.window)
-    llr_edges_train, llr_edges_test = utils.graph_strategy_five(d, args.window)
-    llr_all_edges_train, llr_all_edges_test = utils.graph_strategy_five_all(d, args.window)
-    chi_square_edges_train, chi_square_edges_test = utils.graph_strategy_six(d, args.window)
-    chi_square_all_edges_train, chi_square_all_edges_test = utils.graph_strategy_six_all(d, args.window)
+    strategies_map = { "no_weight": utils.graph_strategy_one,
+                        "pmi_1990": utils.graph_strategy_three,
+                        "pmi_1990_all": utils.graph_strategy_three_all,
+                        "pmi_2019": utils.graph_strategy_two,
+                        "llr": utils.graph_strategy_five,
+                        "llr_all": utils.graph_strategy_five_all }
+    graphs = {}
+    for s in args.strategy:
+        graphs[s] = {}
+        edges_train, edges_test = strategies_map[s](d, args.window)
+        graphs[s]["train"] = edges_train
+        graphs[s]["test"] = edges_test
+
+    #dice_edges_train, dice_edges_test = utils.graph_strategy_four(d, args.window)
+    #dice_all_edges_train, dice_all_edges_test = utils.graph_strategy_four_all(d, args.window)
+    #llr_all_edges_train, llr_all_edges_test = utils.graph_strategy_five_all(d, args.window)
+    #chi_square_edges_train, chi_square_edges_test = utils.graph_strategy_six(d, args.window)
+    #chi_square_all_edges_train, chi_square_all_edges_test = utils.graph_strategy_six_all(d, args.window)
 
     #number of edges of each graph
-    edges_all = count_edges(all_edges_train, all_edges_test)
-    edges_pmi_2019 = count_edges(pmi_2019_edges_train, pmi_2019_edges_test)
-    edges_pmi_1990 = count_edges(pmi_1990_edges_train, pmi_1990_edges_test)
-    edges_pmi_1990_all = count_edges(pmi_1990_all_edges_train, pmi_1990_all_edges_test)
-    edges_dice = count_edges(dice_edges_train, dice_edges_test)
-    edges_dice_all = count_edges(dice_all_edges_train, dice_all_edges_test)
-    edges_llr = count_edges(llr_edges_train, llr_edges_test)
-    edges_llr_all = count_edges(llr_all_edges_train, llr_all_edges_test)
-    edges_chi_square = count_edges(chi_square_edges_train, chi_square_edges_test)
-    edges_chi_square_all = count_edges(chi_square_all_edges_train, chi_square_all_edges_test)
-    plot_boxplot([edges_all, edges_pmi_2019, edges_pmi_1990, edges_pmi_1990_all, edges_dice, edges_dice_all, edges_llr, edges_llr_all, edges_chi_square, edges_chi_square_all], ["Sem peso", "PMI (2019)", "PMI (1990)", "PMI (1990) all", "Dice", "Dice all", "LLR", "LLR all", "Chi-square", "Chi-square all"], args.dataset+"_number_of_edges_"+str(args.window))
+    edges = {}
+    for s in args.strategy:
+        edges[s] = count_edges(graphs[s]["train"], graphs[s]["test"])
 
+    #edges_dice = count_edges(dice_edges_train, dice_edges_test)
+    #edges_dice_all = count_edges(dice_all_edges_train, dice_all_edges_test)
+    #edges_llr = count_edges(llr_edges_train, llr_edges_test)
+    #edges_llr_all = count_edges(llr_all_edges_train, llr_all_edges_test)
+    #edges_chi_square = count_edges(chi_square_edges_train, chi_square_edges_test)
+    #edges_chi_square_all = count_edges(chi_square_all_edges_train, chi_square_all_edges_test)
+    #plot_boxplot([edges_all, edges_pmi_2019, edges_pmi_1990, edges_pmi_1990_all, edges_dice, edges_dice_all, edges_llr, edges_llr_all, edges_chi_square, edges_chi_square_all], ["Sem peso", "PMI (2019)", "PMI (1990)", "PMI (1990) all", "Dice", "Dice all", "LLR", "LLR all", "Chi-square", "Chi-square all"], args.dataset+"_number_of_edges_"+str(args.window))
+
+
+    plot_cost(args.strategy, edges, args.window)
+
+    exit()
     #get number of edges that are small
     edges_sub(edges_all, edges_pmi_2019, "pmi_2019")
     edges_sub(edges_all, edges_pmi_1990, "pmi_1990")
