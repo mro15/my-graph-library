8d344614ec46de41ac5048a7026958a66cd7e9c6
MARCELA RIBEIRO DE OLIVEIRA <mroliveira@urquell>
2020-04-14 19:46:02 -0300

    updated graphics

--- scripts/compare_runner.sh
+++ scripts/compare_runner.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-windows="4 7 20"
+windows="4 7 12 20"
 strategies="no_weight pmi_1990 pmi_1990_all freq freq_all"
 #strategies="no_weight pmi_2019 pmi_1990 dice llr chi_square"
 datasets="webkb polarity 20ng"
@@ -8,6 +8,6 @@ datasets="webkb polarity 20ng"
 
 for w in $windows; do
 	for dr in $datasets; do
-		python3 compare_measures.py --dataset $dr  --strategy no_weight --strategy pmi_1990 --strategy pmi_1990_all --strategy freq --strategy freq_all --window $w --emb_dim 100
+		python3 compare_measures.py --dataset $dr  --strategy pmi_1990 --strategy pmi_1990_all --strategy freq --strategy freq_all --window $w --emb_dim 100
 	done
 done
