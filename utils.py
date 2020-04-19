9bcfd30f9b3a2272fe851267d123abd3858efa13
MARCELA RIBEIRO DE OLIVEIRA <mroliveira@urquell>
2020-04-18 23:40:14 -0300

    change unweighted graph creation]

--- utils.py
+++ utils.py
@@ -22,31 +22,6 @@ def plot_graph(g):
     nx.draw_networkx_edge_labels(g, pos, edge_labels=edge_labels)
     plt.show()
 
-def windows_in_pair(windows):
-    windows_in_pair = Counter()
-    for i in windows:
-        pairs = list(itertools.combinations(i, 2))
-        windows_in_pair.update(pairs)
-    return windows_in_pair
-
-def windows_in_word(windows):
-    windows_in_word = Counter()
-    for i in windows:
-        windows_in_word.update(i)
-    return windows_in_word
-
-def word_count(sentence):
-    return Counter(sentence)
-
-def build_windows(text, k):
-    iterable = iter(text)
-    result = tuple(itertools.islice(iterable, k))
-    if len(result)==k:
-        yield list(result)
-    for element in iterable:
-        result = result[1:] + (element,)
-        yield list(result)
-
 # todo: see a way of not do this
 def all_docs_to_one_tokens_list(d):
     docs = d.train_data+d.test_data
@@ -58,50 +33,57 @@ def all_docs_to_one_tokens_list(d):
 def graph_strategy_one(d, k):
     train_graphs = []
     test_graphs = []
-    print("BUILDING TRAIN GRAPHS")
+    print("BUILDING GRAPHS FROM TRAIN DATASET")
     progress = tqdm(d.train_data)
     for i in progress:
-        windows = []
         g = TextGraph(d.dataset)
-        size = len(i)
-        if size > k:
-            windows += build_windows(i, k)
+        if len(i) > k:
+            windows = bcf.from_words(i, window_size=k)
+            for pairs in windows.ngram_fd.items():
+                w1 = pairs[0][0]
+                w2 = pairs[0][1]
+                g.add_vertex(w1)
+                g.add_vertex(w2)
+                g.add_edge(w1, w2)
         else:
-            windows.append(i)
-        total_windows = len(windows)
-        pair_windows = windows_in_pair(windows)
-        for words, freq in pair_windows.items():
-            w1, w2 = words
-            g.add_vertex(w1)
-            g.add_vertex(w2)
-            g.add_edge(w1, w2)
-        if len(list(pair_windows))<1:
+            if len(i) > 1:
+                windows = bcf.from_words(i, window_size=len(i))
+                for pairs in windows.ngram_fd.items():
+                    w1 = pairs[0][0]
+                    w2 = pairs[0][1]
+                    g.add_vertex(w1)
+                    g.add_vertex(w2)
+                    g.add_edge(w1, w2)
+        if((len(pairs)<1) or (len(g.nodes())==0)):
             g.add_vertex(i[0])
         train_graphs.append(g.graph)
-    print("FINISHED TRAIN GRAPHS")
+    print("FINISHED GRAPHS FROM TRAIN DATASET")
 
-    print("BUILDING TEST GRAPHS")
+    print("BUILDING GRAPHS FROM TEST DATASET")
     progress = tqdm(d.test_data)
     for i in progress:
-        windows = []
         g = TextGraph(d.dataset)
-        size = len(i)
-        if size > k:
-            windows += build_windows(i, k)
+        if len(i) > k:
+            windows = bcf.from_words(i, window_size=k)
+            for pairs in windows.ngram_fd.items():
+                w1 = pairs[0][0]
+                w2 = pairs[0][1]
+                g.add_vertex(w1)
+                g.add_vertex(w2)
+                g.add_edge(w1, w2)
         else:
-            windows.append(i)
-        total_windows = len(windows)
-        pair_windows = windows_in_pair(windows)
-        for words, freq in pair_windows.items():
-            w1, w2 = words
-            g.add_vertex(w1)
-            g.add_vertex(w2)
-            g.add_edge(w1, w2)
-
-        if len(list(pair_windows))<1:
+            if len(i) > 1:
+                windows = bcf.from_words(i, window_size=len(i))
+                for pairs in windows.ngram_fd.items():
+                    w1 = pairs[0][0]
+                    w2 = pairs[0][1]
+                    g.add_vertex(w1)
+                    g.add_vertex(w2)
+                    g.add_edge(w1, w2)
+        if((len(pairs)<1) or (len(g.nodes())==0)):
             g.add_vertex(i[0])
         test_graphs.append(g.graph)
-    print("FINISHED TEST GRAPHS")
+    print("FINISHED GRAPHS FROM TEST DATASET")
 
     return train_graphs, test_graphs
 
