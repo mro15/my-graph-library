1db8ef86e5782c1dfce0d02137d39e58d8b01e34
MARCELA RIBEIRO DE OLIVEIRA <mroliveira@urquell>
2020-05-10 22:35:12 -0300

    results for percentile 90

--- text_graph/features.py
+++ text_graph/features.py
@@ -34,7 +34,7 @@ def discover_sentences_size(sentences):
 
 def sentences_percentile(all_sentences):
     lengths = sentences_len(all_sentences)
-    percent = np.percentile(lengths, 80)
+    percent = np.percentile(lengths, 90)
     print(percent)
     return percent
 
