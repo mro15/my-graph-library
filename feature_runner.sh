c1b24eee60273d741247e421e3715694413bf077
Marcela Ribeiro de Oliveira <mro15@inf.ufpr.br>
2020-08-31 13:35:04 -0300

    update features script

--- feature_runner.sh
+++ feature_runner.sh
@@ -2,21 +2,22 @@
 
 dim="100"
 windows="4 12 20"
-strategies="no_weight pmi pmi_all"
-datasets="polarity webkb r8 20ng"
+strategies="pmi pmi_all llr llr_all chi_square chi_square_all"
+datasets="r8 webkb polarity"
 methods="node2vec"
 cuts="5 10 20"
+time_interval="60"
 
 for d in $dim; do
 	for w in $windows; do
-		for cp in $cuts; do
-			for s in $strategies; do
+		for s in $strategies; do
+			for cp in $cuts; do
 				for dr in $datasets; do
-					python3 feature_generator.py --dataset $dr --strategy $s --window $w --emb_dim $d  --cut_percent $cp &
-					echo "python3 feature_generator.py --dataset $dr --strategy $s --window $w --emb_dim $d --cut_percent $cp" >> executed_features_next_level.txt &
+					mprof run --output time_mem/mprofile.$dr.$s.$w.$cp.dat --interval $time_interval feature_generator.py --dataset $dr --strategy $s --window $w --emb_dim $d  --cut_percent $cp &
+					echo "python3 feature_generator.py --dataset $dr --strategy $s --window $w --emb_dim $d --cut_percent $cp" >> executed_features_new_measures.txt &
 				done
+				wait
 			done
 		done
-		wait
 	done
 done
